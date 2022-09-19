029417c800b3c147e90a53cbc203e205461b6fab
pythond02 <pythond02@WSTEMFA39-20.stu.net.fr.ch>
2022-09-19 16:16:54 +0200

    Fin de l'exercice 9. (Corrigé)

--- Ex09 - JPA_DAO/src/app/App.java
+++ Ex09 - JPA_DAO/src/app/App.java
@@ -47,7 +47,7 @@ public class App extends Application {
         // récupérer une référence sur son contrôleur
         MainCtrl mainCtrl = loader.getController();
 
-        // préparer la première scènesss
+        // préparer la première scène
         Scene scene1 = new Scene(mainView);
 
         // modifier l'estrade pour la première scène
